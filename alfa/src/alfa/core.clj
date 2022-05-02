c94fe82d86745c43ceb53e440d5806356e6a99c9
Sabda PS <sabdaps@gmail.com>
2022-05-02 20:15:32 +0700

    Update core.clj

--- alfa/src/alfa/core.clj
+++ alfa/src/alfa/core.clj
@@ -9,3 +9,9 @@
   (vector
     (= (last col) (lp01 col))
     (= (last []) (lp01 []))))
+
+(defn split-at' [n col]
+  (loop [i 0 [x & xs :as c] col res []]
+    (if (= n i)
+      [res (vec c)]
+      (recur (inc i) xs (conj res x)))))

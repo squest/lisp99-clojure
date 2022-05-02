303c1aa47955668461256431872d5112e6ae09d9
Sabda PS <sabdaps@gmail.com>
2022-05-02 20:10:43 +0700

    problem01

--- alfa/src/alfa/core.clj
+++ alfa/src/alfa/core.clj
@@ -1,3 +1,11 @@
 (ns alfa.core)
 
+(defn lp01 [[x & xs :as col]]
+  (if (empty? xs)
+    x
+    (lp01 xs)))
 
+(defn testing-lp01 [col]
+  (vector
+    (= (last col) (lp01 col))
+    (= (last []) (lp01 []))))

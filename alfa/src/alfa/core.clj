340902fab4d0de3c47c8772dc88ab21bc3c4adb1
Sabda PS <sabdaps@gmail.com>
2022-05-03 16:06:43 +0700

    Update core.clj

--- alfa/src/alfa/core.clj
+++ alfa/src/alfa/core.clj
@@ -1,4 +1,5 @@
-(ns alfa.core)
+(ns alfa.core
+  (:require [clojure.set :as cset]))
 
 (defn lp01 [[x & xs :as col]]
   (if (empty? xs)
@@ -25,3 +26,37 @@
       (if (= i n)
         (recur 1 xs (conj res (conj resi x)) [])
         (recur (inc i) xs res (conj resi x))))))
+
+(defn partition-all' [n col]
+  (loop [[x & xs :as c] col res [] resi []]
+    (if (empty? c)
+      (conj res resi)
+      (if (= (count resi) n)
+        (recur xs (conj res resi) [x])
+        (recur xs res (conj resi x))))))
+
+(defn partition'' [n col]
+  (loop [[x & xs :as c] col res [] resi []]
+    (if (< (+ (count resi) (count c)) n)
+      (seq res)
+      (if (= n (count resi))
+        (recur xs (conj res (seq resi)) [x])
+        (recur xs res (conj resi x))))))
+
+(defn factors [p]
+  (filter #(= 0 (rem p %)) (range 1 (inc p))))
+
+(defn gcd [col]
+  (->> (map factors col)
+       (map set)
+       (apply cset/intersection)
+       vec
+       sort
+       last))
+
+
+
+
+
+
+
